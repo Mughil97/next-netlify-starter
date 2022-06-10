6dbda9f61daffcd0d8791f5121a06e7248e715a9
Mughil97 <mmughil97@gmail.com>
2022-06-10 19:23:15 +0530

    Will it work that easily?

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,7 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+        <p>Tried adding a new line lets see the update :)</p>
       </main>
 
       <Footer />
